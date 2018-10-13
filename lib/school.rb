e3a2dc3e51ad291f7509d26c836208dc8413783f
Yolanda Fay <yolandafay@gmail.com>
2018-10-13 21:07:02 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -2,10 +2,11 @@ class School
 attr_accessor :roster
 attr_reader :school_name, :grade 
 
-@roster = {}
+
 
 def initialize(school_name)
   @school_name = school_name
+  @roster = {}
 end
 
 def add_student(student, grade_level)
